9f0fa5ed10b041a25a04e445f7b8af3251a11482
liujinda <18624366637@163.com>
2016-11-16 14:34:13 +0800

    Initial Commit

---
+++ ipadPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ipadPractice
+//
+//  Created by 刘金大 on 16/11/16.
+//  Copyright © 2016年 Ljd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ipadPractice/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ipadPractice
+//
+//  Created by 刘金大 on 16/11/16.
+//  Copyright © 2016年 Ljd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
